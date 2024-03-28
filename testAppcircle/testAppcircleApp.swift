3390f3c80624a1449a7439a5dfd43242a188149a
boztopuz <boztopuz@gmail.com>
2024-03-28 11:19:42 +0300

    initialCommit

---
+++ testAppcircle/testAppcircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppcircleApp.swift
+//  testAppcircle
+//
+//  Created by Burak Öztopuz on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct testAppcircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
